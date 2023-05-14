48c8d3f68d3728b38a9535fd037932c9e1725ed1
Musa Abraham <profaimusa@gmail.com>
2023-05-14 09:10:45 -0700

    more

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     new_string = []
     for x in my_string:
         if x != 'C' and x != 'c':
-            result.append(x)
-    return ''.join(result)
+            new_string.append(x)
+    return ''.join(new_string)
