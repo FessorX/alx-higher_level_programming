1b9d53a70da37a48a123632acfffd93fed746d60
Musa Abraham <profaimusa@gmail.com>
2023-06-13 11:07:30 -0700

    more

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,13 +4,13 @@
 
 import json
 import sys
-import os.path as path
+import os.path
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-if path.isfile(filename):
+if os.path.isfile(filename):
     obj = load_from_json_file(filename)
 else:
     obj = []
