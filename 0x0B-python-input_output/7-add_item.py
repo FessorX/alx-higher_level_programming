1238ecde73f7b03ed671eb1c723a15b652366fcd
Musa Abraham <profaimusa@gmail.com>
2023-06-13 15:53:45 -0700

    more

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,16 +3,20 @@
 
 
 import json
-import sys
+from sys import argv
 import os.path
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json = __import__('5-save_to_json_file').save_to_json_file
+load_from_json = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 if os.path.isfile(filename):
-    obj = load_from_json_file(filename)
+    obj = load_from_json(filename)
 else:
     obj = []
-obj.extend(sys.argv[1:])
-save_to_json_file(obj, filename)
+count = 0
+fro item in argv:
+    if count != 0:
+        obj.append(item)
+    count += 1
+save_to_json(obj, filename)
